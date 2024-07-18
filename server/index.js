cead6e1dec82d28ad20d38a4d3476be23562f88c
seonyeonghun <kjca1117@naver.com>
2024-07-18 14:43:01 +0900

    모듈설치 및 라우팅 추가

--- server/index.js
+++ server/index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + "/public/home.html");
+    res.sendFile(__dirname + "/public/home.html"); // 99% index.html
+});
+app.get('/main', function(req, res) {
+    res.sendFile(__dirname + "/public/main.html");
+});
+app.get('/contact', function(req, res) {
+    res.sendFile(__dirname + "/public/contact.html");
 });
 
 app.listen(3000);
