911a201b311665b6c95b443a0591f638370a5873
chulhee <solroya@gamil.com>
2024-07-25 17:04:16 +0900

    로그인 페이지 구현

--- server/index.js
+++ server/index.js
@@ -10,21 +10,21 @@ app.use(express.urlencoded({extended: true}));
 
 const users = [
     {
-        idx : 1,
+        idx: 1,
         id: "test",
         pw: "1234",
-        created: new DataTransfer("2024-07-20"),
+        created: new Date("2024-07-20"),
         email: "test@naver.com",
         nick: "겁나 무서운 전사"
     },
     {
-        idx : 2,
+        idx: 2,
         id: "hello",
         pw: "hello1004",
-        created: new DataTransfer("2024-07-22"),
-        email: "hello@naver.com",
+        created: new Date("2024-07-22"),
+        email: "helloworld@naver.com",
         nick: "헬로월드"
-    },
+    }
 ];
 
 app.get('/', function(req, res) {
@@ -33,26 +33,53 @@ app.get('/', function(req, res) {
 app.get('/main', function(req, res) {
     res.sendFile(__dirname + "/public/main.html");
 });
-app.get('/chart', function(req, res){
+app.post('/login', function(req, res) {
+    let {user_id, user_pw } = req.body; // 각각 id, pw 정보를 나누어서 저장
+
+    let foundUser = users.filter(user => user.id == user_id && user.pw == user_pw);
+
+    console.log(foundUser)
+    // console.log(user_id, user_pw);
+    // res.status(201).send("데이터를 수신했습니다.")
+    if(user_id == "test" && user_pw == "1234"){
+        res.json({
+            message: "login ok",
+            status : 200,
+            isLogin: true,
+            user: foundUser.nick,
+            rediect_url: "/home"
+        })
+    }else {
+        res.json({
+            message: "login fail",
+            status : 400,
+            isLogin: false,
+            user: null,
+            rediect_url: "/home"
+        })
+    } 
+
+    // 기본 회원 정보와 전송받은 정보를 비교 --> 일치 : 로그인 성공, 불일치 : 로그인 실패
+    // 첫 페이지로 이동시키거나, 경고를 하거나..후속조치
+})
+app.get('/chart', function(req, res) {
     res.json([
         {
-            ranking : 1,
-            title : "데드풀과 울버린",
-            satisfy : 99,
-            ratio : 44.4,
+            ranking: 1,
+            title: '데드풀과 울버린',
+            satisfy: 99,
+            ratio: 44.4,
+            img: ['images/poster1.jpg', 'images/poster2.jpg'],
             url: '/movie?cate=action&sf?code={ranking}'
         },
         {
-            ranking : 2,
-            title : "파일럿",
-            satisfy : 99,
-            ratio : 15.4,
+            ranking: 2,
+            title: '파일럿',
+            satisfy: 99,
+            ratio: 15.7,
             url: '/movie?cate=action&sf?code={ranking}'
-        }
+        },
     ])
-})
-app.post('/login', function(req, res) {
-    console.log(req.body);
 })
 // req: request [요청]
 // res: response [응답]
